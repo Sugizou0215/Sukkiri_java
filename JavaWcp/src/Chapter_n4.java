be221ff8b6d7ec81484331cfe0332078314ffa8a
sugizou0215 <sugizou0215@gmail.com>
2021-11-19 14:38:08 +0900

    finish n4

---
+++ JavaWcp/src/Chapter_n4.java
@@ -0,0 +1,31 @@
+
+public class Chapter_n4 {
+	public static void main(String[] args) {
+		// 4-1
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
+		
+		// 4-2
+		int[] moneyList = {121902, 8302, 55100};
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+		for (int j : moneyList) {
+			System.out.println(j);
+		}
+		
+		//4-3
+		
+		int[] numbers = {3, 4, 9};
+		System.out.println("1桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		
+		for (int k : numbers) {
+			if (k == input) {
+				System.out.println("アタリ！");
+			}
+		}
+	}
+}
