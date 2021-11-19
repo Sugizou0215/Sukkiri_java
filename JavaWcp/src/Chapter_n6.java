f31d5baa7bb1c8fca7b5559e18c17d622902388e
sugizou0215 <sugizou0215@gmail.com>
2021-11-19 19:29:42 +0900

    finish n6

---
+++ JavaWcp/src/Chapter_n6.java
@@ -0,0 +1,10 @@
+import chapter_n6.comment.Zenhan;
+
+public class Chapter_n6 {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		chapter_n6.comment.Kouhan.callDeae();
+		chapter_n6.comment.Kouhan.showMondokoro();
+	}
+}
