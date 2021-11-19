c7dac731ee7f611eff1da0c7e2ca2c4838a1a772
sugizou0215 <sugizou0215@gmail.com>
2021-11-19 11:24:18 +0900

    finish n1

---
+++ JavaWcp/src/Chapter_n1.java
@@ -0,0 +1,21 @@
+
+public class Chapter_n1 {
+	public static void main(String[] args ) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+	    System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は" + c);
+	    
+	    boolean check = true;
+	    char character = '駆';
+	    double pi = 3.14;
+	    long number = 314159265853979L;
+	    String sentence = "ミナトの攻撃！敵に15ポイントのダメージを与えた";
+	    System.out.println(check);
+	    System.out.println(character);
+	    System.out.println(pi);
+	    System.out.println(number);
+	    System.out.println(sentence);
+	    
+	}
+}
