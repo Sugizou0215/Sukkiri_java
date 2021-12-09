08d523f72ead99c5b303503980daac41b9d101a3
sugizou0215 <sugizou0215@gmail.com>
2021-12-09 19:03:14 +0900

    finish n17.5

---
+++ JavaWcp/src/Chapter_n17.java
@@ -0,0 +1,27 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Chapter_n17 {
+	public static void main(String[] args) {
+		FileWriter fw = null;
+		
+		try {
+			fw = new FileWriter("data.txt");
+			fw.write("hello,world!");
+		} catch(IOException e) {
+			System.out.println("書き込みエラー:" + e.getMessage());
+		} catch(NullPointerException e) {
+			System.out.println("nullエラー:" + e.getMessage());
+		} catch(Exception e) {
+			System.out.println("何らかのエラー:" + e.getMessage());
+		} finally {
+			if (fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					;
+				}
+			}		
+		}
+	}
+}
